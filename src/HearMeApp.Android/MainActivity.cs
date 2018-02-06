4f4bde399a8cf974f881720b082ce118e64f02bf
Yehor Hromadskyi <yhrom@softserveinc.com>
2018-02-06 18:25:30 +0200

    Playing sound on muted device

--- src/HearMeApp.Android/MainActivity.cs
+++ src/HearMeApp.Android/MainActivity.cs
@@ -2,13 +2,17 @@
 using Android.Widget;
 using Android.OS;
 using Android.Media;
+using Android.Content;
+using Java.Lang;
 
 namespace HearMeApp.Android
 {
     [Activity(Label = "Hear me", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, MediaPlayer.IOnCompletionListener
     {
         MediaPlayer _mediaPlayer;
+        AudioManager _audioService;
+        int _userVolume;
 
         public Button PlayButton { get; set; }
 
@@ -23,11 +27,28 @@ namespace HearMeApp.Android
             PlayButton.Click += OnPlayButtonClick;
 
             _mediaPlayer = MediaPlayer.Create(this, Resource.Raw.sax);
+            _mediaPlayer.SetAudioAttributes(new AudioAttributes.Builder()
+                                                               .SetUsage(AudioUsageKind.Media)
+                                                               .SetContentType(AudioContentType.Music)
+                                                               .Build());
+
+            _mediaPlayer.SetOnCompletionListener(this);
+
+            _audioService = (AudioManager)GetSystemService(AudioService);
+
+            _userVolume = _audioService.GetStreamVolume(Stream.Music);
         }
 
         private void OnPlayButtonClick(object sender, System.EventArgs e)
         {
+            _audioService.SetStreamVolume(Stream.Music, _audioService.GetStreamMaxVolume(Stream.Music), VolumeNotificationFlags.PlaySound);
+
             _mediaPlayer.Start();
+        }
+
+        public void OnCompletion(MediaPlayer mp)
+        {
+            _audioService.SetStreamVolume(Stream.Music, _userVolume, VolumeNotificationFlags.PlaySound);
         }
 
         protected override void OnDestroy()
